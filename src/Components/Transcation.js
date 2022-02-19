64b2fc4d060880e3b2b3e5cc5d36b3340fae5020
rawand1998 <rawandgaradh@gmail.com>
2022-02-19 09:47:07 +0200

    craete context file

--- src/Components/Transcation.js
+++ src/Components/Transcation.js
@@ -1,24 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
  const Transcation = () => {
+   const [text,setText]=useState('')
+   const [amount,setAmount]=useState(0)
+   console.log(text,amount)
   return (
     <div>
       <h3>Add new transaction</h3>
       <form>
         <div className="form-control">
           <label for="text">Text</label>
-          <input type="text" id="text" placeholder="Enter text..." />
+          <input type="text" id="text" value={text}  onChange={(e)=>{setText(e.target.value)}} placeholder="Enter text..." />
         </div>
         <div className="form-control">
           <label for="amount"
             >Amount <br />
             (negative - expense, positive - income)</label
           >
-          <input type="number" placeholder="Enter amount..." />
+          <input type="number" placeholder="Enter amount..."  value={amount} onChange={(e)=>{setAmount(e.target.value)}} />
         </div>
         <button className="btn">Add transaction</button>
       </form>
     </div>
   )
 }
+
 export default Transcation;
